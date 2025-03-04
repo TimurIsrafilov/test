e709b6eb5a536bf3723f1ada310df9e9412c2b6d
TIMUR <timooooon11@yandex.ru>
2025-03-05 01:14:56 +0300

    Updates

---
+++ assets/SpeakerPage-CdpzLoGQ.js
@@ -0,0 +1,1 @@
+import{_ as t,c as r,o as e,f as a,d as o}from"./index-Cn-vm5M4.js";const s={},_={class:"container"};function p(c,n){return e(),r("section",_,"Speaker")}const f=t(s,[["render",p]]),d=a({__name:"SpeakerPage",setup(c){return(n,i)=>(e(),o(f))}});export{d as default};
